70bb98fe0b77f347fad58a6a03ba607c622b3d24
Jorman <jorman.iisrael@me.com>
2022-01-11 14:22:17 -0500

    update

--- server/server.js
+++ server/server.js
@@ -4,13 +4,16 @@ const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
+const { authMiddleware } = require('./utils/auth');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 const startServer = async () => {
   const server = new ApolloServer({
     typeDefs,
-    resolvers
+    resolvers,
+    context: authMiddleware
+   
   });
   await server.start();
   server.applyMiddleware({ app });
